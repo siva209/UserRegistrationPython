d582ba264b76c764435f4eddf451e0f0b2a50569
siva209 <sivanarayanayeruva209@gmail.com>
2020-12-01 10:02:23 +0530

    [siva]:Added valid user last name

--- com/bridgelabz/UserRegistration.py
+++ com/bridgelabz/UserRegistration.py
@@ -16,6 +16,19 @@ class UserRegistration:
         else:
                 print("Invalid input, Please enter the valid name")
 
+     #User LastName
+
+    def userlastname(self):
+        pattern2 = '^[a-z]{3,}[A-Z]{1}$'
+        user_name = input("Enter the User Last Name : ")
+        result = re.match(pattern2, user_name)
+        if result:
+            print("Valid Name")
+
+        else:
+            print("Invalid Name, Please enter proper name")
+
 if __name__ == "__main__":
     user = UserRegistration()
     user.userfirstname()
+    user.userlastname()
