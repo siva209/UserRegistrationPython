5290e84fdfaf1788b4701cfb84808f1691d4a0db
siva209 <sivanarayanayeruva209@gmail.com>
2020-12-01 09:57:05 +0530

    [siva]:Added valid user first name

--- com/bridgelabz/UserRegistration.py
+++ com/bridgelabz/UserRegistration.py
@@ -1,2 +1,21 @@
+
+import re
+
 class UserRegistration:
-    print("Welcome to User Registration")
+
+
+    # User First Name
+    def userfirstname(self):
+
+        pattern1 = '^[A-Z]{1}[a-z]{3,}$'
+        user_name = input("Enter the User First Name : ");
+        result = re.match(pattern1,user_name)
+        if result:
+         print("Valid Name")
+
+        else:
+                print("Invalid input, Please enter the valid name")
+
+if __name__ == "__main__":
+    user = UserRegistration()
+    user.userfirstname()
